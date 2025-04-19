9c2a30e1778d8e1bdac5fd556706c2191fd695b2
emretahtali <149873315+emretahtali@users.noreply.github.com>
2025-04-20 02:22:06 +0300

    page setup

---
+++ jotform-hackathon/src/components/MainTopbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="topbar">
+    <div class="nav-links">
+      <router-link to="/" class="topbar-link">Home</router-link>
+      <router-link to="/about" class="topbar-link">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainTopbar'
+};
+</script>
+
+<style scoped>
+.topbar {
+  display: flex;
+  justify-content: center; /* Center the content horizontally */
+  align-items: center;
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+}
+
+.nav-links {
+  display: flex;
+  justify-content: center; /* Center the links within this container */
+}
+
+.topbar-link {
+  color: white;
+  text-decoration: none;
+  font-size: 1rem;
+  margin-left: 20px;
+  padding: 8px 16px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+}
+
+.topbar-link:hover {
+  background-color: #ff9800;
+  color: white;
+  transform: scale(1.05);
+  text-decoration: none;
+}
+
+@media (max-width: 768px) {
+  .topbar {
+    flex-direction: column;
+    align-items: center; /* Center items vertically on small screens */
+    padding: 10px 20px;
+  }
+
+  .topbar-item {
+    margin: 10px 0;
+    font-size: 16px;
+  }
+}
+</style>
---
+++ jotform-hackathon/src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page_container">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+};
+</script>
+
+<style scoped>
+.page_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #e0f7fa;
+}
+</style>
